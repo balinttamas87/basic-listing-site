9836fbacfac18923338baac95f33f69a2da0b3ca
Tamas Balint <balint.tamas87@gmail.com>
2020-09-19 22:32:05 +0100

    Display logo and input side-by-side and style them

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useState } from "react";
 
 function Home(props) {
   const link = props.link;
   console.log({ link: props.link });
+
+  const [searchInputValue, setSearchInputValue] = useState(""); 
+
+  const onSearchValueChange = (e) => {
+    setSearchInputValue(e.target.value);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,8 +23,9 @@ function Home(props) {
         <h2 className={styles.title}>
           Search user
         </h2>
-        <div>
+        <div className={styles["search-wrapper"]} style={{ display: "flex" }}>
           <img src="/assets/algolia-blue-mark.svg" alt="algolia-blue-mark"/>
+          <input className={styles["search-input"]}type="text" value={searchInputValue} onChange={onSearchValueChange}/>
         </div>
       </main>
       <a href={link}>{link}</a>
