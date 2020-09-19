faf33f1decb67e6710f2885c1dc72ad1070745e2
Tamas Balint <balint.tamas87@gmail.com>
2020-09-20 00:31:31 +0100

    Delete unneceassy files

--- configure-search.js
+++
@@ -1,9 +0,0 @@
-import { AuthMode } from '@algolia/client-common';
-
-const algoliasearch = algoliasearch(
-  'YourApplicationID',
-  'dee646598d711bfc8ca326b526bd40f9',
-  {
-    authMode: AuthMode.WithinHeaders, // or AuthMode.WithinQueryParameters
-  },
-);
