d27e5f4ecc50b02ebb28ecb457de6acd08897071
Tamas Balint <balint.tamas87@gmail.com>
2020-09-20 22:33:24 +0100

    Delete '>' from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import useRouter from "next/router";
 import Link from "next/link";
 
 function Home(props) {
   return (
     <>
       <h1>Home page</h1>
-      <Link href="/search">Click here to navigate to Search page `{'-->'}</Link>
+      <Link href="/search">Click here to navigate to Search page</Link>
     </>
   );
 };
